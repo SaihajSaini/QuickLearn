56183958bdf18e9c39ad3a061794c3cf1520924c
rsingh135 <rsingh.ala03@gmail.com>
2025-07-31 16:11:49 -0500

    Stop tracking build artifacts and local database files

--- frontend/.next/static/chunks/src_app_[locale]_(marketing)_page_tsx_4cf004d0._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_ae642d01._.js"
-],
-    source: "dynamic"
-});
